b748bc2a2df8a3565ddd5cf943d7b8f4de89de67
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2022-05-24 22:46:13 +0400

    ...

---
+++ assets/js.js
@@ -0,0 +1,39 @@
+var number1="46";
+var number2="4";
+var result=number1+number2;
+console.log(result);
+
+var number1="46";
+var number2="4";
+var result=parseInt(number1)+parseInt(number2);
+console.log(result);
+
+var number1="47.45";
+var number2="0.87";
+var result=parseFloat(number1)+parseFloat(number2);
+console.log(result);
+
+var num1="hello";
+var num2="22";
+var result=isNaN(num1);
+console.log(result);
+
+var strSum=prompt("sheiyvanet konkretuli tanxa",8000);
+var strPercent=prompt("sheiyvanet konkretuli procenti");
+var sum=parseInt(strSum);
+var percent=parseInt(strPercent);
+sum=sum+sum*percent/100;
+alert("procentis daricxvis shemdeg tanxa iqneba",+sum)
+
+var people=[
+    ["tom",29,false],["ani",24,true],["alice",34,false]
+]
+console.log (people[0])
+
+var income=500;
+var age=20;
+if(income<550 && age>21) {
+    var message="shemosavali metia 870-ze an ravii"
+alert(message);
+}
+
